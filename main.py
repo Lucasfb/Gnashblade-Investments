14f666bfbd81069727dbd7956524200306c30705
lucas <Lucas1214fb@gmail.com>
2021-09-11 22:25:58 -0300

    Program now works with full listings for a item instead of only the current buy and sell values. Also added a bar graph for buy listings, but it was found to not be very useful. This part is commented out

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 if __name__ == '__main__':
     import requests
     import json
+    import datetime
+    import matplotlib.pyplot as plt
+    import numpy as np
 
     # Loading the data of items of interest from a file
     # Data is manually added to the file
@@ -9,23 +12,50 @@ if __name__ == '__main__':
     materials = json.load(f)
     f.close()
 
-    base_url = "https://api.guildwars2.com/v2/"
-    endpoint_selected = "commerce/prices"
+    # Selecting the "listings" endpoint
+    base_url = "https://api.guildwars2.com/v2"
+    endpoint_selected = "commerce/listings"
 
     # This test will only get the buy and sell prices of Mithril Ore
     desired_item = materials["mithril"]
     desired_id = desired_item["id"]
 
-    full_endpoint_path = base_url + endpoint_selected + "?ids=" + desired_id
+    full_endpoint_path = base_url + "/" + endpoint_selected + "?ids=" + desired_id
+
     response = requests.get(full_endpoint_path)
-    current_prices = json.loads(response.content)
+    # Considers the difference between requesting and getting the current time negligible
+    request_time = datetime.datetime.now()
+    request_timestamp = request_time.timestamp()
+    current_listings = json.loads(response.content)
+    current_listings = current_listings[0]
+    current_listings['timestamp'] = request_timestamp
+    current_listings['request_time'] = request_time
     # print(response.json())
 
+
     # Showing the results in a pretty way
-    item_buy_price = current_prices[0]['buys']['unit_price']
-    item_sell_price = current_prices[0]['sells']['unit_price']
+    item_buy_price = current_listings['buys'][0]['unit_price']
+    item_sell_price = current_listings['sells'][0]['unit_price']
 
     print("-------------------------------")
     print(desired_item['name'])
-    print("Buy: ", item_buy_price , "|", "Sell: ", item_sell_price)
+    print("  Highest Buy Price: ", item_buy_price)
+    print("  Lowest Sell Price: ", item_sell_price)
     print("-------------------------------")
+
+
+    # # Bar graph of current listings
+    # ## Bar graph of buy listings
+    # buy_values = []
+    # buy_quantities = []
+    # for listing in current_listings['buys']:
+    #     buy_values.append(listing['unit_price'])
+    #     buy_quantities.append(listing['quantity'])
+    # fif, ax = plt.subplots()
+    # ax.barh(buy_values, buy_quantities, align='center')
+    # ax.set_yticks(np.arange(0, max(buy_values)+2, 1))
+    # #ax.set_xlim(right=15)  # adjust xlim to fit labels
+    # plt.ylabel("Buy price (copper)")
+    # plt.xlabel("Quantity listed")
+    # plt.title("Number of listings for all buy price")
+    # plt.show()
