84c50a01b797ac3068bb5057cfafc19bc94033f7
lucas <Lucas1214fb@gmail.com>
2021-10-30 17:15:17 -0300

    Changed the file so it can be run as a script instead of having to import and run a function

--- create_and_configure_database.py
+++ create_and_configure_database.py
@@ -18,45 +18,44 @@ def create_table(conn,create_table_SQL):
     except Error as e:
         print(e)
 
-def create_database():
-    db_file = r".\material_listings.db"
+db_file = r".\material_listings.db"
 
-    sql_create_items_table = """CREATE TABLE IF NOT EXISTS items (
-    item_id integer PRIMARY KEY,
-    item_name TEXT NOT NULL, 
-    material_type TEXT,
-    material_tier INTEGER
-    );"""
+sql_create_items_table = """CREATE TABLE IF NOT EXISTS items (
+item_id integer PRIMARY KEY,
+item_name TEXT NOT NULL, 
+material_type TEXT,
+material_tier INTEGER
+);"""
 
-    sql_create_listings_table = """ CREATE TABLE IF NOT EXISTS listings (
-    time TEXT,
-    item_id INTEGER NOT NULL,
-    buy_price INTEGER NOT NULL,
-    buy_number_of_listings INTEGER NOT NULL, 
-    buy_quantity INTEGER NOT NULL,
-    sell_price INTEGER NOT NULL,
-    sell_number_of_listings INTEGER NOT NULL,
-    sell_quantity INTEGER NOT NULL,
-    full_buy_list BLOB NOT NULL,
-    full_sell_list BLOB NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES items (item_id)
-    );"""
+sql_create_listings_table = """ CREATE TABLE IF NOT EXISTS listings (
+time TEXT,
+item_id INTEGER NOT NULL,
+buy_price INTEGER NOT NULL,
+buy_number_of_listings INTEGER NOT NULL, 
+buy_quantity INTEGER NOT NULL,
+sell_price INTEGER NOT NULL,
+sell_number_of_listings INTEGER NOT NULL,
+sell_quantity INTEGER NOT NULL,
+full_buy_list BLOB,
+full_sell_list BLOB,
+FOREIGN KEY (item_id) REFERENCES items (item_id)
+);"""
 
-    f = open('materials.json')
-    materials = json.load(f)
-    f.close()
+f = open('materials.json')
+materials = json.load(f)
+f.close()
 
-    conn = create_connection(db_file)
-    if conn is not None:
-        create_table(conn,sql_create_items_table)
-        create_table(conn, sql_create_listings_table)
-    else:
-        print("Error! cannot create the database connection.")
+conn = create_connection(db_file)
+if conn is not None:
+    create_table(conn,sql_create_items_table)
+    create_table(conn, sql_create_listings_table)
+else:
+    print("Error! cannot create the database connection.")
 
-    for item in materials.items():
-        item_to_insert = (item[1]['id'],item[1]['name'],item[1]['type'],item[1]['tier'])
-        sql_insert_item = ''' INSERT INTO items(item_id,item_name,material_type,material_tier)
-                     VALUES(?,?,?,?) '''
-        cur = conn.cursor()
-        cur.execute(sql_insert_item, item_to_insert)
-        conn.commit()
+for item in materials.items():
+    item_to_insert = (item[1]['id'],item[1]['name'],item[1]['type'],item[1]['tier'])
+    sql_insert_item = ''' INSERT INTO items(item_id,item_name,material_type,material_tier)
+                 VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql_insert_item, item_to_insert)
+    conn.commit()
