9bc3d64e201a45159ccb4f8cfc99a51387d4ec7c
lucas <Lucas1214fb@gmail.com>
2022-05-13 19:50:05 -0300

    Script to update request_time and request_timestamp from old format to new one

---
+++ database_timestamp_conversion.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import sqlite3
+
+
+# Connecting to the local database
+conn = sqlite3.connect('material_listings.db')
+cur = conn.cursor()
+
+rows = cur.execute("SELECT request_time,request_timestamp FROM listings").fetchall()
+
+for each_time in rows:
+    (old_request_time,old_request_timestamp) = each_time
+    new_request_time = datetime.fromisoformat(old_request_time).astimezone().isoformat()
+    new_request_timestamp = int(float(old_request_timestamp)) # Convert from string to float, then to int
+    print(old_request_timestamp,new_request_timestamp)
+    cur.execute("UPDATE listings SET request_time = ?, request_timestamp = ? WHERE request_time= ?", [new_request_time, new_request_timestamp, old_request_time])
+    conn.commit()
+
+conn.close()
