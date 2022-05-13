5c20bae561077b03188b9d715eba3d2913d99ae0
lucas <Lucas1214fb@gmail.com>
2022-05-13 20:03:44 -0300

    Removed print statement used for debug. Statement was previously only commented out

--- database_timestamp_conversion.py
+++ database_timestamp_conversion.py
@@ -12,7 +12,6 @@ for each_time in rows:
     (old_request_time,old_request_timestamp) = each_time
     new_request_time = datetime.fromisoformat(old_request_time).astimezone().isoformat()
     new_request_timestamp = int(float(old_request_timestamp)) # Convert from string to float, then to int
-    ## print(old_request_timestamp,new_request_timestamp)
     cur.execute("UPDATE listings SET request_time = ?, request_timestamp = ? WHERE request_time= ?", [new_request_time, new_request_timestamp, old_request_time])
     conn.commit()
 
