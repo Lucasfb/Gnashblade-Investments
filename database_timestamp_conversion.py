401fc2898851c55c842d901ac15acdb405e938e5
lucas <Lucas1214fb@gmail.com>
2022-05-13 20:07:13 -0300

    Formatting changed

--- database_timestamp_conversion.py
+++ database_timestamp_conversion.py
@@ -9,10 +9,11 @@ cur = conn.cursor()
 rows = cur.execute("SELECT request_time,request_timestamp FROM listings").fetchall()
 
 for each_time in rows:
-    (old_request_time,old_request_timestamp) = each_time
+    (old_request_time, old_request_timestamp) = each_time
     new_request_time = datetime.fromisoformat(old_request_time).astimezone().isoformat()
-    new_request_timestamp = int(float(old_request_timestamp)) # Convert from string to float, then to int
-    cur.execute("UPDATE listings SET request_time = ?, request_timestamp = ? WHERE request_time= ?", [new_request_time, new_request_timestamp, old_request_time])
+    new_request_timestamp = int(float(old_request_timestamp))  # Convert from string to float, then to int
+    cur.execute("UPDATE listings SET request_time = ?, request_timestamp = ? WHERE request_time= ?",
+                [new_request_time, new_request_timestamp, old_request_time])
     conn.commit()
 
 conn.close()
