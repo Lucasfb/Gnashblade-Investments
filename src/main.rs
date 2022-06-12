ce47fe45e7ffe7af56bfbda4b9efac61a3f65e2c
Lucasfb <lucas1214fb@gmail.com>
2022-06-11 21:14:05 -0300

    Added debug configuration and gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use chrono::prelude::*;
+use chrono::{prelude::*};
 use error_chain::error_chain;
-//use reqwest;
+use reqwest;
 use rusqlite::{params,Connection};
 use serde_derive::{Deserialize, Serialize};
 use std::fs;
@@ -47,9 +47,18 @@ fn get_material_ids_from_database (db:&Connection) -> Result<Vec<String>> {
     Ok(list_of_item_ids)
 }
 
+fn get_name_from_item_id(item_id: String, material_list: &Vec<Material>) -> &String{
+    for item in material_list {
+        if item.id == item_id {
+            return &item.name;
+        }
+    }
+    panic!("Could not find the name for the selected ID")
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+
     let materials = fs::read_to_string("materials.json").expect("Unable to read file");
     let materials_list: Vec<Material> =
         serde_json::from_str(&materials).expect("Cannot parse JSON");
@@ -78,17 +87,36 @@ async fn main() -> Result<()> {
     // Doesn't waste time with multiple insertions
     let tx = db.transaction().unwrap();
 
-    for material in materials_list {
-        let url_request = format!("{}/{}?ids={}", base_url, endpoint_selected, material.id);
-        let request_time: DateTime<Local> = Local::now();
-        let request_timestamp = request_time.timestamp();
-        let response = reqwest::get(&url_request).await.unwrap();
 
-        let listings: Vec<MaterialListing> = response.json::<Vec<MaterialListing>>().await.unwrap();
-        // println!("Material: {:?}", material.name);
-        // println!("ID: {:?}", listings[0].id);
-        // println!("Buy price:{:?}", listings[0].buys[0].unit_price);
-        // println!("Sell price:{:?}", listings[0].sells[0].unit_price);
+    // Get the TP data for each material and insert them into the database
+    // API request is made for all items at once
+    // Database inserts are made for each individual item but commited only at the end
+    let mut url_request = format!("{}/{}?ids=", base_url, endpoint_selected);
+    let request_time: DateTime<Local> = Local::now();
+    let request_timestamp = request_time.timestamp();
+
+    // Generating the URL for the API request
+    for material in &materials_list {
+        url_request.push_str(&material.id);
+        url_request.push(',');
+    }
+
+    //let time_before_API_request :DateTime<Local> = Local::now();
+    let response = reqwest::get(&url_request).await?;
+    //let time_after_API_request :DateTime<Local> = Local::now();
+
+    //let time_before_API_response_processing = Local::now();
+    let listings: Vec<MaterialListing> = response.json::<Vec<MaterialListing>>().await.unwrap();
+    //let time_after_API_response_processing = Local::now();
+
+    //print!("API response received");
+
+    //let time_before_SQL_insert :DateTime<Local> = Local::now();
+    for material_listing in listings {
+        // println!("Material: {:?}", get_name_from_item_id(material_listing.id.to_string(), &materials_list));
+        // println!("ID: {:?}", material_listing.id);
+        // println!("Buy price:{:?}", material_listing.buys[0].unit_price);
+        // println!("Sell price:{:?}", material_listing.sells[0].unit_price);
         // println!();
 
         let sql_insert_item = "INSERT INTO listings (request_time,request_timestamp,item_id,buy_price,buy_number_of_listings,buy_quantity,sell_price,sell_number_of_listings,sell_quantity) VALUES (?,?,?,?,?,?,?,?,?)";
@@ -97,21 +125,36 @@ async fn main() -> Result<()> {
             params![
                 request_time.to_rfc3339(),
                 request_timestamp,
-                material.id,
-                listings[0].buys[0].unit_price,
-                listings[0].buys[0].listings,
-                listings[0].buys[0].quantity,
-                listings[0].sells[0].unit_price,
-                listings[0].sells[0].listings,
-                listings[0].sells[0].quantity,
+                material_listing.id,
+                material_listing.buys[0].unit_price,
+                material_listing.buys[0].listings,
+                material_listing.buys[0].quantity,
+                material_listing.sells[0].unit_price,
+                material_listing.sells[0].listings,
+                material_listing.sells[0].quantity,
             ],
         )
         .unwrap();
     }
+    //let time_after_SQL_insert :DateTime<Local> = Local::now();
 
+
+    //let time_before_SQL_commit :DateTime<Local> = Local::now();
     tx.commit().unwrap();
+    //let time_after_SQL_commit :DateTime<Local> = Local::now();
+
     db.close().unwrap();
-   // let _debug_point = 1;
+
+
+    // let request_duration = time_after_API_request - time_before_API_request;
+    // let request_processing_duration = time_after_API_response_processing - time_before_API_response_processing;
+    // let insert_duration = time_after_SQL_insert - time_before_SQL_insert;
+    // let commit_duration = time_after_SQL_commit - time_before_SQL_commit;
+
+    // println!("API Request duration: {}",request_duration.num_milliseconds());
+    // println!("API Request processing duration: {}",request_processing_duration.num_milliseconds());
+    // println!("SQL insert loop duration: {}",insert_duration.num_milliseconds());
+    // println!("SQL commit duration: {}",commit_duration.num_milliseconds());
 
    Ok(())
 }
