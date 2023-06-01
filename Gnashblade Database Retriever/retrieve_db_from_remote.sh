c5b05899c37c7c330348399494d072621bc7d016
lucas <lucas1214fb@gmail.com>
2023-06-01 20:30:04 -0300

    Script to retrieve DB from remote raspberry pi

---
+++ Gnashblade Database Retriever/retrieve_db_from_remote.sh
@@ -0,0 +1,13 @@
+remote_host=raspberrypi # Connected with Tailscale, so no explicit IP is necessary
+remote_username=gw2stuff
+remote_file_location="/home/"$remote_username"/rust_tp_collector/material_listings.db"
+
+# Copy the file
+scp $remote_username@$remote_host:$remote_file_location ./
+
+# Rename to include the current timestamp
+time_of_file=$(date +%Y%m%d%H%M%S)
+old_filename='material_listings' # Not including the .db extension
+new_filename=$old_filename'_'$time_of_file'.db'
+
+mv material_listings.db databases/material_listings_$time_of_file.db
